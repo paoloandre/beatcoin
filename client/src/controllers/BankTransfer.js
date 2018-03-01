fb3375d940f7c5d2e66010b86d7f2ac9ee06867d
Paolo Andreassi <pablo15941@gmail.com>
2018-03-01 16:37:04 +0100

    add card selection

--- client/src/controllers/BankTransfer.js
+++ client/src/controllers/BankTransfer.js
@@ -1,4 +1,34 @@
 angular.module('beatCoin')
-  .controller('BankTransferCtrl', function($scope, $http, Card) {
+  .controller('BankTransferCtrl', function($scope, $http, toastr, Card, Account, $rootScope) {
+
+    $scope.getProfile = function() {
+      Account.getProfile()
+        .then(function(response) {
+          $scope.user = response.data;
+          $rootScope.currentUser = response.data;
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
+    };
+
+    $scope.getCards = function() {
+      var i = 0;
+      Card.getCards()
+        .then(function(response) {
+          $scope.cards = response.data;
+          // $scope.cards.selected = false;
+          for (i=0; i < $scope.cards.length; i++) {
+              $scope.cards[i].selected = false;
+          }
+          console.log($scope.cards);
+        })
+        .catch(function(response) {
+          toastr.error(response.data.message, response.status);
+        });
+    };
+
+    $scope.getProfile();
+    $scope.getCards();
 
   });
